d9c7a78ca416409af9e51f3d53edddbb6d0df413
uopkq <xufangyu709@gmail.com>
2018-04-05 09:53:41 +0200

    ass1

--- hellocube/mainwindow.cpp
+++ hellocube/mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
+#include <QStatusBar>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    createMenuBar();
+    this->statusBar()->showMessage("Set label text.");
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void MainWindow::createMenuBar()
+{
+    menuBar = new QMenuBar();
+    fileMenu = new QMenu("&File");
+    exitAction = new QAction("E&xit" , fileMenu);
+    exitAction->setShortcut(QKeySequence::Quit);
+    fileMenu->addAction(exitAction);
+    menuBar->addMenu(fileMenu);
+    setMenuBar(menuBar);
 }
